8a2b1275d672783890d28cdd03f9e1b1283810cd
b84an <ivan.bertotto@gmail.com>
2021-08-31 13:49:54 +0200

    Update scriptone.sh

--- scriptone.sh
+++ scriptone.sh
@@ -1,4 +1,41 @@
-#sudo su
+#!/bin/bash
+
+#                               u,
+#                               E[
+#                        ._aawgmCmgwwa,,
+#                     _amBQWmWmBCWmBmmWWmw,.
+#                  .amWWmBBmWmWBCWmWBWmWmBWma,
+#                 ammmmmWmWBBWmW[WmWmWmWmWmWWQa,
+#               _mmmWmWWmWmWmBBW(BWmWmWmWmWmBmmmc
+#              <WmBWmWmBBBBBWmWm(XBBBBBBBWmWmWBBma
+#             <WmWmBBBWmWmWmWmWD.]BWmWmWmWmWmWmWBWL
+#            =WBWmWmWmWmWmWmWmW[-:WmWmWmWmWmWmWmBmWL
+#           .mWmBBBWmWmWmWmWmWZ-::]WmWmWmWmWmWmWBWmm;
+#           jmBWmWmWmWmWmWmWmW':::.4WmWmWmWmWmWmBBBWm
+#           mBWmWmWmWmWmWmWmW(_i_<=%SWmWmWmWmWmWmWmWm;
+#          =WmBBBBBWmWmWmBBBC+++++++<WmWmWmBBBBBBBWmWL
+#          ]mWBWmWmWmBBBBWmWC-::::::<WmWmWmWmWmWmWmWmk
+#          ]BBBmWmWmWmWmWmW#6iiiiiii%#WmWmWmWmWmWmWmBE
+#          ]mWmWmWmWmWmWmWmm;. . . . wWmWmWmWmWmWmWmWk
+#          -WmWmWmWmWmWmWmB(....-..-.-9WmWmWmWmWmWmWmf
+#           $WmWmWmWmWmWmP`...-._:...:."QmWmWmWmWmWmW'
+#           ]WmWmWmWmWmB! ......m/......-$mWmWmWmWmWZ
+#            $mWmWmWmW#^.......]Wh.......-4WmWmBBBBm'
+#            +#WmWmWmW'... . . mmW/.. .... 4WmWmWmW[
+#             )#WmWmW'.. . .. -""!~. .. . .-$mWmWmf
+#              -WmBW[ . . . . ...   .  . . .-WmWm!
+#               -4WZ  .  . . __. .__. . . .  ]BD^
+#                 3' .  .    mE`  ?W[       . S`
+#                 2 .       ]F  _. "C    .    ];
+#                <[         -   ?'  -          L
+#                ]            _s_s,.           3
+#                """""""""""""" . """"""""""!""?
+# Builder Creator Script
+# Trasforma il tuo Elementary 6 in un AvoLinux
+# By Ivan.bertotto@gmail.com
+# Rilasciato sotto licenza GplV2
+
+
 
 
 # FUNZIONI
